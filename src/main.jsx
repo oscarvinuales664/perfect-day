fb75992a0c49775c76ffac104aff3ac5268a1656
oscarvinuales664 <oscarvinuales664@gmail.com>
2026-06-13 14:04:12 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+mi-dia-perfecto/
+├── index.html          ← archivo "index"
+├── package.json        ← archivo "package"
+├── vite.config.js      ← archivo "vite.config"
+└── src/
+    ├── main.jsx        ← archivo "main"
+    └── App.jsx         ← archivo "mi-dia-perfecto" (renómbralo App.jsx)
